f79aee23d2f8bd9c1e105b9d908f4e9b1dfe63f1
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-04-29 13:26:08 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
     
       < Navbar />
-      {/* <HeroService></HeroService> */}
+      <HeroService></HeroService>
       <Hero />
       <Whowe />
       <Qualities />
