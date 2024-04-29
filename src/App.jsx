734b5fd6a689180a5e642bd9a1913feaf590a7ea
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-04-29 14:39:18 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,39 @@ import Hero from './components/hero/Hero'
 import Footer from "./components/footer/Footer"
 import Whowe from "./components/whowe/Whowe"
 import HeroService from "./components/servicePage_components/HeroService/HeroService"
+import services from './components/servicePage_components/service.json'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from "react-router-dom";
+import Home from "./components/home/Home"
+
 
 function App() {
   return (
-    <>
+
+    <Router>
+
+   
+    <div>
+
     
-      < Navbar />
-      <HeroService></HeroService>
-      <Hero />
-      <Whowe />
-      <Qualities />
-      <Services />
+    <Navbar></Navbar>
+    <Routes>
+
+
+      <Route path="/" element={ <Home/>} />
+      <Route path="/Private_Limited" element={ <HeroService data={services.Private_Limited_Company_Registration}/>} />
+      
+ 
+    </Routes>
       <Footer />
-    </>
+    </div>
+     
+    </Router>
+    
   )
 }
 
--- src/components/servicePage_components/HeroService/HeroService.jsx
+++ src/components/servicePage_components/HeroService/HeroService.jsx
@@ -2,59 +2,30 @@ import React from "react";
 import { SiTicktick } from "react-icons/si";
 import "./HeroService.css";
 
-const HeroService = () => {
+const HeroService = ({ data }) => {
   return (
     <div className="HeroService">
       <div className="HeroServiceHeading">
-        <span>Private Limited</span> Company Registration
+        <span>Private_Limited</span> Company Registration
       </div>
-      <div className="bootom">
-        Private limited company registration in Jaipur Starting at Just
-        ₹9,000/-* only
-      </div>
+      <div className="bootom">{data.tagline}</div>
       <div className="features">
-        <div className="feature">
-          <SiTicktick className="tickicon" />
-          <div className="featureLine"> Ideal for start-ups</div>
-        </div>
-        <div className="feature">
-          <SiTicktick />
-          <div className="featureLine"> Easy Transferability</div>
-        </div>
-        <div className="feature">
-          <SiTicktick />
-          <div className="featureLine"> Simple & Secure Online Process</div>
-        </div>
-        <div className="feature">
-          <SiTicktick />
-          <div className="featureLine"> Dedicated Professional</div>
-        </div>
-        <div className="feature">
-          <SiTicktick />
-          <div className="featureLine"> Takes 5-7 Days</div>
-        </div>
-        <div className="feature">
-          <SiTicktick />
-          <div className="featureLine">Get Post Incorporation Assistance</div>
-        </div>
+        {data.features.map((feature, i) => {
+          return (
+            <div className="feature" key={i}>
+              <SiTicktick className="tickicon" />
+              <div className="featureLine">{feature}</div>
+            </div>
+          );
+        })}
       </div>
 
       <div className="Serviceintroduction">
         <div className="ServiceIntroHeading">
-          <span>INTRO</span>DUCTION 
+          <span>INTRO</span>DUCTION
         </div>
         <div className="paragraphintro">
-          Introduction of Private Limited Company For starting a business in
-          India Private Limited Company is the most popular and an effective
-          medium for higher growth aspirants. It is incorporated under Companies
-          Act, 2013 and has various benefits as it ensures limited liability and
-          separate legal entity which means safeguarding of the personal
-          property. This type of entity is mainly preferred by the start ups and
-          the growing entities. So, if you are willing to start a Company your
-          first priority is to get it registered Once the same is registered it
-          increases its authenticity as well as offers various benefits which
-          can be offering liability protection to protect the Company's assets,
-          attraction of more and more funds etc.
+         {data.introduction}
         </div>
       </div>
     </div>
