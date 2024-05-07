b38c3e927fa69675dded6f43cbf3a7e5c151049e
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-05-07 23:38:09 +0530

    change

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import './Hero.css'
+import 'animate.css';
 
 const Hero = () => {
     
@@ -6,13 +7,13 @@ const Hero = () => {
         <div className='hero'>
             <div className="heroSection">
                 <div className="heroContainer">
-                    <div className="heading">
+                    <div className="heading animate__animated animate__backInDown">
                         <h1><span>Legal Yodha</span> Your Trusted CA Consultancy Partner.</h1>
                     </div>
                     <div className="ssHeading">
                         <h5>Empowering Success, Ensuring Compliance</h5>
                     </div>
-                    <div className="subHeading">
+                    <div className="subHeading  animate__animated animate__backInUp">
                         <h4>Strategic Solutions, Trusted Guidance: Elevate your financial journey with our CA consultancy expertise. </h4>
                     </div>
                     <div className="btns">
