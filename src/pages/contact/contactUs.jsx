c3ac7189f7d24fbe4da056dade80230d3a725857
Chinmay Jain <chinmayluhadiya62@gmail.com>
2024-05-08 23:45:54 +0530

    commit

--- src/pages/contact/contactUs.jsx
+++ src/pages/contact/contactUs.jsx
@@ -3,7 +3,7 @@ import './contact.css';
 
 function ContactUs() {
   return (
-    <>
+    <div className='contact'>
         <section id="page-header">
         <h2>
             #let's_talk
@@ -39,7 +39,7 @@ function ContactUs() {
         </div>
         <div classname="map">
             {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7777.998662786958!2d77.64154817427921!3d12.907764213590964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15400539b081%3A0xd2ab89c277c8e8df!2s1stop!5e0!3m2!1sen!2sin!4v1659879093781!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> */}
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.844500552668!2d77.1901374748188!3d28.544393288078965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1df6be7e6a75%3A0x8b149c3feb567bb1!2sIIT%20Delhi%20Main%20Rd%2C%20IIT%20Campus%2C%20New%20Delhi%2C%20Delhi%20110016!5e0!3m2!1sen!2sin!4v1714565640724!5m2!1sen!2sin" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.844500552668!2d77.1901374748188!3d28.544393288078965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1df6be7e6a75%3A0x8b149c3feb567bb1!2sIIT%20Delhi%20Main%20Rd%2C%20IIT%20Campus%2C%20New%20Delhi%2C%20Delhi%20110016!5e0!3m2!1sen!2sin!4v1714565640724!5m2!1sen!2sin" width="400" height="300" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 
@@ -55,7 +55,7 @@ function ContactUs() {
         </form>
    </section>
 
-    </>
+    </div>
   )
 }
 
